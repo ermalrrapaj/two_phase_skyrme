5fd0afe2bdaac1baab12633390ea0c4a3c8dd9cc
Luke Roberts <lroberts@tapir.caltech.edu>
2015-08-21 12:42:30 -0700

    Add some code for finding and sorting phase boundary in a more reasonable way

--- Skyrme_EOS_cpp/src/gibbs_test.cpp
+++ Skyrme_EOS_cpp/src/gibbs_test.cpp
@@ -1,5 +1,8 @@
 #include <iostream> 
 #include <math.h> 
+#include <vector> 
+#include <utility>
+#include <algorithm> 
 
 #include "EOSData.hpp" 
 #include "EOSSkyrme.hpp" 
@@ -10,8 +13,10 @@ int main() {
   EOSSkyrme eos;
   GibbsPhaseConstruct gibbs(eos);
   double TMeV = 2.0; 
-  double delta = 0.01;
-   
+  double delta = 0.05;
+
+  std::vector<std::pair<EOSData,EOSData>> phaseBound;
+  
   // Scan over neutron chemical potentials 
   double NpLoG = 1.e-20;
   double NpHiG = 0.08;
@@ -23,19 +28,12 @@ int main() {
       outDat = gibbs.FindPhasePoint(TMeV/Constants::HBCFmMeV, 
           mun/Constants::HBCFmMeV, NpLoG, NpHiG); 
     } catch (...) {
-      std::cerr << mun <<  " failed.\n";
+      //std::cerr << mun <<  " failed.\n";
       continue;
     }
-    
-    if (outDat[0].Nn()>outDat[0].Np()) {
-      std::cout << outDat[0].Nn() << " "; 
-      std::cout << outDat[1].Nn() << " "; 
-      std::cout << outDat[0].Np() << " "; 
-      std::cout << outDat[1].Np() << " ";
-      std::cout << outDat[1].P()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mun()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mup()*Constants::HBCFmMeV << std::endl;
-    } 
+         
+    if (outDat[0].Nn()>outDat[0].Np()) 
+        phaseBound.push_back(std::pair<EOSData, EOSData>(outDat[0], outDat[1]));
      
     NpLoG = outDat[0].Np()*0.95;
     NpHiG = outDat[1].Np()*1.05;
@@ -51,19 +49,12 @@ int main() {
       outDat = gibbs.FindPhasePoint(TMeV/Constants::HBCFmMeV, 
           mun/Constants::HBCFmMeV, NpLoG, NpHiG); 
     } catch (...) {
-      std::cerr << mun <<  " failed.\n";
+      //std::cerr << mun <<  " failed.\n";
       continue;
     }
     
-    if (outDat[0].Nn()>outDat[0].Np()) {
-      std::cout << outDat[0].Nn() << " "; 
-      std::cout << outDat[1].Nn() << " "; 
-      std::cout << outDat[0].Np() << " "; 
-      std::cout << outDat[1].Np() << " ";
-      std::cout << outDat[1].P()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mun()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mup()*Constants::HBCFmMeV << std::endl;
-    } 
+    if (outDat[0].Nn()>outDat[0].Np()) 
+        phaseBound.push_back(std::pair<EOSData, EOSData>(outDat[0], outDat[1]));
      
     NpLoG = outDat[0].Np()*0.95;
     NpHiG = outDat[1].Np()*1.05;
@@ -80,20 +71,12 @@ int main() {
       outDat = gibbs.FindPhasePoint(TMeV/Constants::HBCFmMeV, 
           mup/Constants::HBCFmMeV, NnLoG, NnHiG, false); 
     } catch (...) {
-      std::cerr << mup <<  " failed.\n";
+      //std::cerr << mup <<  " failed.\n";
       continue;
     }
 
-    if (outDat[0].Nn()<outDat[0].Np()) {
-      std::cout << outDat[0].Nn() << " "; 
-      std::cout << outDat[1].Nn() << " "; 
-      std::cout << outDat[0].Np() << " "; 
-      std::cout << outDat[1].Np() << " ";
-      std::cout << outDat[1].P()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mun()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mup()*Constants::HBCFmMeV << std::endl;
-    }
-     
+    if (outDat[0].Nn()<outDat[0].Np()) 
+        phaseBound.push_back(std::pair<EOSData, EOSData>(outDat[0], outDat[1]));
     NnLoG = outDat[0].Nn()*0.95;
     NnHiG = outDat[1].Nn()*1.05;
   } 
@@ -108,24 +91,31 @@ int main() {
       outDat = gibbs.FindPhasePoint(TMeV/Constants::HBCFmMeV, 
           mup/Constants::HBCFmMeV, NnLoG, NnHiG, false); 
     } catch (...) {
-      std::cerr << mup <<  " failed.\n";
+      //std::cerr << mup <<  " failed.\n";
       continue;
     }
 
-    if (outDat[0].Nn()<outDat[0].Np()) {
-      std::cout << outDat[0].Nn() << " "; 
-      std::cout << outDat[1].Nn() << " "; 
-      std::cout << outDat[0].Np() << " "; 
-      std::cout << outDat[1].Np() << " ";
-      std::cout << outDat[1].P()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mun()*Constants::HBCFmMeV << " ";
-      std::cout << outDat[1].Mup()*Constants::HBCFmMeV << std::endl;
-    }
+    if (outDat[0].Nn()<outDat[0].Np())
+        phaseBound.push_back(std::pair<EOSData, EOSData>(outDat[0], outDat[1]));
      
     NnLoG = outDat[0].Nn()*0.95;
     NnHiG = outDat[1].Nn()*1.05;
   }
   
+  std::sort (phaseBound.begin(), phaseBound.end(), 
+      [](std::pair<EOSData, EOSData> a, std::pair<EOSData, EOSData> b) { 
+        return ((a.first).Mun() < (b.first).Mun());});
+  
+  for (auto &a : phaseBound) {
+    std::cout << (a.first).Nn() << " "; 
+    std::cout << (a.second).Nn() << " "; 
+    std::cout << (a.first).Np() << " "; 
+    std::cout << (a.second).Np() << " ";
+    std::cout << (a.first).P()*Constants::HBCFmMeV << " ";
+    std::cout << (a.first).Mun()*Constants::HBCFmMeV << " ";
+    std::cout << (a.first).Mup()*Constants::HBCFmMeV << std::endl;
+  }  
+   
   return 0;
 }
 
