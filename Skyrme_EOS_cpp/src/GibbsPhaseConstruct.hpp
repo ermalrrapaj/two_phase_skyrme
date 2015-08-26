da1ffdc32ae019367f71161dfa55bee7fcf80695
Luke Roberts <lroberts@tapir.caltech.edu>
2015-08-26 13:35:22 -0700

    Add documentation to Gibbs phase header file

--- Skyrme_EOS_cpp/src/GibbsPhaseConstruct.hpp
+++ Skyrme_EOS_cpp/src/GibbsPhaseConstruct.hpp
@@ -24,19 +24,22 @@ public:
   /// Initialize with an EoS that has a non-convex region
   GibbsPhaseConstruct(const EOSBase& eos);  
   
+  
+  EOSData FromNAndT(const EOSData& eosIn) const;
+  
+  /// This function is not implemented and will throw an error if called 
   std::vector<EOSData> FromMuAndT(const EOSData& eosIn) const {
     throw std::logic_error("FromMuAndT has not been implemented.");
     return std::vector<EOSData>();
   } 
   
-  EOSData FromNAndT(const EOSData& eosIn) const;
-   
-  
+  /// This function is not implemented and will throw an error if called 
   EOSData FromNpMunAndT(const EOSData& eosIn) const {
     throw std::logic_error("FromNpMunAndT has not been implemented.");
     return EOSData();
   } 
   
+  /// This function is not implemented and will throw an error if called 
   EOSData FromNnMupAndT(const EOSData& eosIn) const {
     throw std::logic_error("FromNnMupAndT has not been implemented.");
     return EOSData();
@@ -51,6 +54,8 @@ public:
       double NLoG=1.e-20, double NHiG=0.08, double deltaMu=0.05) const;
   
 protected:
+  /// Solve the three Gibbs equilibrium equations and the constraint equations 
+  /// for the neutron and proton density and return the mixed phase state
   EOSData GetState(const EOSData& eosIn, const EOSData& lo, const EOSData& hi, 
       double ug) const;
   
