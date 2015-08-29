5aff9f27b37438d774c5e67c5352b7bee8a9c2fa
Luke Roberts <lroberts@tapir.caltech.edu>
2015-08-29 13:45:21 -0700

    Start on single nucleus approximation Eos inheriting from Gibbs EOS

---
+++ Skyrme_EOS_cpp/src/EOSSingleNucleus.hpp
@@ -0,0 +1,31 @@
+/// \file EOSSingleNucleus.hpp
+/// \author lroberts
+/// \since Aug 29, 2015
+///
+/// \brief
+///
+///
+
+#ifndef EOS_EOSSINGLENUCLEUS_HPP_
+#define EOS_EOSSINGLENUCLEUS_HPP_
+
+#include <memory> 
+#include <vector> 
+#include <utility> 
+
+#include "EOSBase.hpp"
+#include "EOSData.hpp"
+#include "GibbsPhaseConstruct.hpp"
+
+///
+/// Calculate a two-phase EOS within the single nucleus 
+/// approximation.
+///
+class EOSSingleNucleus : public GibbsPhaseConstruct {
+public:
+  EOSSingleNucleus(const EOSBase& eos) : GibbsPhaseConstruct(eos) {};
+
+protected:
+};
+
+#endif // EOS_EOSSINGLENUCLEUS_HPP_
