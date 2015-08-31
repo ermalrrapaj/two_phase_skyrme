fbc85ec1127a704fd0b61e5da52259f5460ad453
Luke Roberts <lroberts@tapir.caltech.edu>
2015-08-31 13:59:47 -0700

    Calculate over a range of pressures

--- Skyrme_EOS_cpp/src/gibbs_test.cpp
+++ Skyrme_EOS_cpp/src/gibbs_test.cpp
@@ -1,4 +1,5 @@
-#include <iostream> 
+#include <iostream>
+#include <fstream> 
 #include <math.h> 
 #include <vector> 
 #include <utility>
@@ -14,25 +15,35 @@ int main() {
   double HBC = Constants::HBCFmMeV;
   
   EOSSkyrme eos;
-  GibbsPhaseConstruct gibbs(eos);
+  GibbsPhaseConstruct gibbs(eos, false);
   
-  double ye = 0.3; 
+  std::ifstream ifs("eos_data.xml");
+  boost::archive::text_iarchive ia(ifs); 
+  ia >> gibbs;
+  ifs.close();
   
-  for (double lT = log10(0.1); lT < log10(30.0); lT += 0.05) {
-    double T  = pow(10.0, lT)/HBC;
-    for (double lnb = -8.0; lnb<0.5; lnb += 0.005) {  
-      double nb = pow(10.0, lnb); 
-      EOSData stateGibbs = 
-          gibbs.FromNAndT(EOSData::InputFromTNbYe(T, nb, ye));
-      
-      EOSData stateBulk = 
-          eos.FromNAndT(EOSData::InputFromTNbYe(T, nb, ye));
-      
-      std::cout << nb << " " << T << " " << stateGibbs.P() << " " << stateBulk.P(); 
-      std::cout << " " << stateGibbs.S() << " " << stateBulk.S() << std::endl;
+  for (double lye = -2.0; lye <= log10(0.5); lye += 0.1) { 
+    double ye = pow(10.0, lye); 
+    std::ofstream outFile; 
+    outFile.open("State" + std::to_string(ye) + ".out"); 
+    
+    for (double lT = log10(0.1); lT < log10(30.0); lT += 0.05) {
+      double T  = pow(10.0, lT)/HBC;
+      for (double lnb = -8.0; lnb<0.5; lnb += 0.005) {  
+        double nb = pow(10.0, lnb); 
+        EOSData stateGibbs = 
+            gibbs.FromNAndT(EOSData::InputFromTNbYe(T, nb, ye));
+        
+        EOSData stateBulk = 
+            eos.FromNAndT(EOSData::InputFromTNbYe(T, nb, ye));
+        
+        outFile << nb << " " << T << " " << stateGibbs.P() << " " << stateBulk.P(); 
+        outFile << " " << stateGibbs.S() << " " << stateBulk.S() << std::endl;
+      }
+      outFile << std::endl;
+      outFile << T*HBC << " done \n";
     }
-    std::cout << std::endl;
-    std::cerr << T*HBC << " done \n";
+    outFile.close(); 
   }
   return 0;
    
