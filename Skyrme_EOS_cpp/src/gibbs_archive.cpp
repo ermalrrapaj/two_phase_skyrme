10d67133acddcefdc82a78ec873f8debb3025909
Luke Roberts <lroberts@tapir.caltech.edu>
2015-09-10 23:16:56 -0700

    Test serialization and pressure tracks with Ska

--- Skyrme_EOS_cpp/src/gibbs_archive.cpp
+++ Skyrme_EOS_cpp/src/gibbs_archive.cpp
@@ -15,7 +15,10 @@
 int main() {
   double HBC = Constants::HBCFmMeV;
   
-  EOSSkyrme eos;
+	std::vector<double> Ska35S2009{-172.485, 172.087, -1767.71, 0.282732, 
+      12899.2, 0.413266, 0.35};
+  EOSSkyrme eos = EOSSkyrme::FromErmalSkyrme(Ska35S2009);
+  //EOSSkyrme eos;
   EOSSingleNucleus gibbs(eos);
   std::ofstream ofs("eos_data.xml"); 
   boost::archive::text_oarchive oa(ofs); 
--- Skyrme_EOS_cpp/src/gibbs_test.cpp
+++ Skyrme_EOS_cpp/src/gibbs_test.cpp
@@ -14,7 +14,9 @@
 int main() {
   double HBC = Constants::HBCFmMeV;
   
-  EOSSkyrme eos;
+	std::vector<double> Ska35S2009{-172.485, 172.087, -1767.71, 0.282732, 
+      12899.2, 0.413266, 0.35};
+  EOSSkyrme eos = EOSSkyrme::FromErmalSkyrme(Ska35S2009);
   EOSSingleNucleus eosSN(eos, false);
   GibbsPhaseConstruct gibbs(eos, false);
   
@@ -37,7 +39,7 @@ int main() {
     for (double lT = log10(0.1); lT < log10(30); lT += 0.25) {
       double T  = pow(10.0, lT)/HBC;
       std::cout << "Point : " << ye << " " << T << std::endl;
-      for (double lnb = -6.0; lnb<log10(0.15); lnb += 0.005) {  
+      for (double lnb = -6.0; lnb<log10(0.15); lnb += 0.01) {  
         double nb = pow(10.0, lnb); 
         EOSData stateGibbs = 
             gibbs.FromNAndT(EOSData::InputFromTNbYe(T, nb, ye));
@@ -60,64 +62,11 @@ int main() {
         outFile << " " << stateSN.S() << " " << u << std::endl;
       }
       outFile << std::endl;
-      outFile << T*HBC << " done \n";
+      std::cout << T*HBC << " done \n";
     }
     outFile.close(); 
   }
   return 0;
    
-  for (double T = 0.01/HBC; T <= 0.1/HBC; T += 0.01/HBC) {
-    auto phaseBound = gibbs.FindFixedTPhaseBoundary(T); 
-    
-    std::cerr << T * HBC << " " << phaseBound.size() << std::endl; 
-    if (phaseBound.size() > 0) {
-      // Write out the phase boundary
-      std::cout << "#[1] Nn_1 (fm^-3)" << std::endl; 
-      std::cout << "#[2] Nn_2 (fm^-3)" << std::endl; 
-      std::cout << "#[3] Np_1 (fm^-3)" << std::endl; 
-      std::cout << "#[4] Np_2 (fm^-3)" << std::endl; 
-      std::cout << "#[5] P (MeV fm^-3)" << std::endl; 
-      std::cout << "#[6] Mun (MeV fm^-3)" << std::endl; 
-      std::cout << "#[7] Mup (MeV fm^-3)" << std::endl; 
-      std::cout << "#[8] T (MeV)" << std::endl; 
-      
-      for (auto &a : phaseBound) {
-        std::cout << (a.first).Nn() << " "; 
-        std::cout << (a.second).Nn() << " "; 
-        std::cout << (a.first).Np() << " "; 
-        std::cout << (a.second).Np() << " ";
-        std::cout << (a.first).P()*HBC << " ";
-        std::cout << (a.first).Mun()*HBC << " ";
-        std::cout << (a.first).Mup()*HBC << " ";
-        std::cout << (a.first).T()*HBC << std::endl;
-      }
-      
-       
-      //std::reverse(phaseBound.begin(), phaseBound.end());  
-      //
-      //for (auto &a : phaseBound) {
-      //  std::cout << (a.second).Nn() << " "; 
-      //  std::cout << (a.first).Nn() << " "; 
-      //  std::cout << (a.second).Np() << " ";
-      //  std::cout << (a.first).Np() << " "; 
-      //  std::cout << (a.first).P()*HBC << " ";
-      //  std::cout << (a.first).Mun()*HBC << " ";
-      //  std::cout << (a.first).Mup()*HBC << " ";
-      //  std::cout << (a.first).T()*HBC << std::endl;
-      //}
-      //std::cout << (phaseBound.back().first).Nn() << " "; 
-      //std::cout << (phaseBound.back().second).Nn() << " "; 
-      //std::cout << (phaseBound.back().first).Np() << " ";
-      //std::cout << (phaseBound.back().second).Np() << " "; 
-      //std::cout << (phaseBound.back().first).P()*HBC << " ";
-      //std::cout << (phaseBound.back().first).Mun()*HBC << " ";
-      //std::cout << (phaseBound.back().first).Mup()*HBC << " ";
-      //std::cout << (phaseBound.back().first).T()*HBC << std::endl;
-      
-      std::cout << std::endl; 
-    } 
-  }   
-    
-  return 0;
 }
 
