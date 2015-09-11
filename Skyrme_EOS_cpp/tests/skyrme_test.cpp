ace4f335b9cf1f00d8d9eb12acbfafc726f229ea
Luke Roberts <lroberts@tapir.caltech.edu>
2015-09-11 15:09:40 -0700

    Start on Skyrme tests

--- Skyrme_EOS_cpp/tests/skyrme_test.cpp
+++ Skyrme_EOS_cpp/tests/skyrme_test.cpp
@@ -2,38 +2,55 @@
 #include <math.h> 
 #include <vector>
 
+#include "Util/Constants.hpp"
 #include "EquationsOfState/EOSData.hpp" 
 #include "EquationsOfState/EOSSkyrme.hpp" 
 #include "EquationsOfState/GibbsPhaseConstruct.hpp"
 #include "EquationsOfState/SkyrmeParameters.hpp"
 
 int main() {
-
+  const double HBC = Constants::HBCFmMeV;
+  
 /* Test for whether the conversion is right. -> I got the right numbers!	
-	double HBC=197.3;
+	double HBC=HBC;
   EOSSkyrme eos2 =EOSSkyrme:: FromErmalSkyrme (0.0, 0.0, -1048.53, -0.0914293, 15488.0, -0.5, 1.002), eos;
   std::cout<<-286.1/HBC<<", "<<-107.1/HBC<<", "<<968.0/HBC<<", "<<0<<", "<<0<<", "<<0<<", "<<2.002<<"\n";
  */
 	
-  static constexpr std::array<const double, 7> Ska35S2009 = {{-172.485, 172.087, -1767.71, 0.282732, 12899.2, 0.413266, 0.35}};
- 
-  EOSSkyrme eos2 = EOSSkyrme::FromErmalSkyrme(Ska35S2009);
+  EOSSkyrme eos2 = EOSSkyrme::FromErmalSkyrme(ErmalSkyrmeParameters::Ska35S2009);
   EOSSkyrme eos4 = EOSSkyrme::FromSaturation(SaturationSkyrmeParameters::Ska35S2009);
 
-  EOSData eosIn = EOSData::InputFromTNnNp(5.0/197.3, 0.05, 0.05); 
+  EOSData eosIn = EOSData::InputFromTNnNp(5.0/HBC, 0.05, 0.05); 
 	EOSData state2 = eos2.FromNAndT(eosIn);
   EOSData state4 = eos4.FromNAndT(eosIn);
-    
+
+  // Check that the two ways of getting the parameters work    
   std::cout << state2.Np() << " " << state2.Nn() << " " << state2.Ye() << " "
-  << state2.T()*197.3 << " " << state2.E()*197.3 << " " << state2.S()  << " "
-  << state4.Mun()*197.3 << " " << state4.Mup()*197.3  << " " 
-  << state4.P()*197.3 << " " << std::endl;
+  << state2.T()*HBC << " " << state2.E()*HBC << " " << state2.S()  << " "
+  << state4.Mun()*HBC << " " << state4.Mup()*HBC  << " " 
+  << state4.P()*HBC << " " << std::endl;
   
   std::cout << state4.Np() << " " << state4.Nn() << " " << state4.Ye() << " "
-  << state4.T()*197.3 << " " << state4.E()*197.3 << " " << state4.S() << " " 
-  << state4.Mun()*197.3 << " " << state4.Mup()*197.3 << " " 
-  << state4.P()*197.3 << " " << std::endl;
-
+  << state4.T()*HBC << " " << state4.E()*HBC << " " << state4.S() << " " 
+  << state4.Mun()*HBC << " " << state4.Mup()*HBC << " " 
+  << state4.P()*HBC << " " << std::endl;
+  
+  const double tol = 1.e-2; // We seem to only get about 1% agreement
+  if (fabs(state4.E()/state2.E() - 1.0)>tol) return 1;
+  if (fabs(state4.S()/state2.S() - 1.0)>tol) return 1;
+  if (fabs(state4.P()/state2.P() - 1.0)>tol) return 1;
+  if (fabs(state4.Mun()/state2.Mun() - 1.0)>tol) return 1;
+  if (fabs(state4.Mup()/state2.Mup() - 1.0)>tol) return 1;
+  
+  // Check that we can succesfully find a chemical potential 
+  EOSData forward = eos2.FromNAndT(eosIn); 
+  EOSData reverse = eos2.FromNnMupAndT(
+      EOSData::InputFromTNnMup(forward.T(), forward.Nn(), forward.Mup()));
+  std::cout << "Mu test : " << reverse.Np() << " " << forward.Np() << std::endl;
+  if (fabs(reverse.Np()/forward.Np() - 1.0) > tol) return 1;
+  
+  // Check that we can succesfully find an entropy 
+   
   return 0;
 }
 
