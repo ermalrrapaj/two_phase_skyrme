e0cfede46e2783d3e08c9a6038782603ad29e041
Luke Roberts <lroberts@tapir.caltech.edu>
2015-10-01 16:41:51 -0700

    Correct derivatives of specific quantities

--- Skyrme_EOS_cpp/src/EquationsOfState/EOSElectron.cpp
+++ Skyrme_EOS_cpp/src/EquationsOfState/EOSElectron.cpp
@@ -28,31 +28,26 @@ EOSData EOSElectron::FromNAndT(const EOSData& eosIn) {
   double sarr[2][2]; 
   g = 2.0;
   __fermi_dirac_MOD_two_fermion_eos(&g, &me, &T, &np, muarr, parr, earr, sarr);
-  mu = muarr[0][0]; 
-  p = parr[0][0]; 
-  e = earr[0][0]; 
-  s = sarr[0][0]; 
-   
+  
   EOSData eosOut;
   eosOut.Set("T", eosIn.T()); 
   eosOut.Set("Np", eosIn.Np()); 
   eosOut.Set("Nn", eosIn.Nn()); 
   
-  eosOut.Set("P", p); 
-
+  eosOut.Set(   "P", parr[0][0]); 
   eosOut.SetDNp("P", parr[0][1]);  
   eosOut.SetDT( "P", parr[1][0]);  
   eosOut.SetDNn("P", 0.0);  
   
   double nb = eosIn.Nb();
-  eosOut.Set("S", sarr[0][0]/nb);
-  eosOut.SetDNp("S", sarr[0][1]/nb - sarr[0][0]/(nb*nb));  
-  eosOut.SetDT( "S", sarr[1][0]/nb);  
+  eosOut.Set(   "S",  sarr[0][0]/nb); 
+  eosOut.SetDNp("S",  sarr[0][1]/nb - sarr[0][0]/(nb*nb));  
+  eosOut.SetDT( "S",  sarr[1][0]/nb);  
   eosOut.SetDNn("S", -sarr[0][0]/(nb*nb));  
-  
-  eosOut.Set("E", e/nb); 
-  eosOut.SetDNp("E", earr[0][1]/nb - earr[0][0]/(nb*nb));  
-  eosOut.SetDT( "E", earr[1][0]/nb);  
+
+  eosOut.Set(   "E",  earr[0][0]/nb); 
+  eosOut.SetDNp("E",  earr[0][1]/nb - earr[0][0]/(nb*nb));  
+  eosOut.SetDT( "E",  earr[1][0]/nb);  
   eosOut.SetDNn("E", -earr[0][0]/(nb*nb));  
 
   
