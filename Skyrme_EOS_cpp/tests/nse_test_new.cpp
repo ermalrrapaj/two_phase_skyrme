1e325f9617e86b2ff017e47746339ed8d67585a3
Luke Roberts <lroberts@tapir.caltech.edu>
2015-12-10 15:55:53 -0800

    Clean up and set up for comparison between static and non-static

--- Skyrme_EOS_cpp/tests/nse_test_new.cpp
+++ Skyrme_EOS_cpp/tests/nse_test_new.cpp
@@ -16,31 +16,41 @@ int main() {
   const double HBC = Constants::HBCFmMeV;
   
   EOSSkyrme eos; // = EOSSkyrme::FreeGas();
-  //EOSSkyrme eosInside; 
+  EOSSkyrme eosInside; 
    
   std::vector<std::unique_ptr<NucleusBase>> nuclei; 
-  //nuclei.push_back(StaticNucleus(28, 56, 56.0*8.0/HBC, {}, {}, 9.2*56.0 ).MakeUniquePtr());
-  for (int i=53; i<78; i++) {
-    //nuclei.push_back(LDNucleus(28, i, eosInside).MakeUniquePtr());
-    //nuclei.push_back(StaticNucleus(27, i, i*7.5/HBC, {}, {}, 1.0*i/0.16 ).MakeUniquePtr());
-    nuclei.push_back(StaticNucleus(27, i, i*7.3/HBC, {}, {}, 0.7*i/0.16 ).MakeUniquePtr());
-    nuclei.push_back(StaticNucleus(28, i, i*8.0/HBC, {}, {}, 0.7*i/0.16 ).MakeUniquePtr());
-    nuclei.push_back(StaticNucleus(29, i, i*7.5/HBC, {}, {}, 0.7*i/0.16 ).MakeUniquePtr());
-    nuclei.push_back(StaticNucleus(50, i, i*8.0/HBC, {}, {}, 0.7*i/0.16 ).MakeUniquePtr());
+  std::vector<std::unique_ptr<NucleusBase>> nucleiStatic; 
+  for (int Z=28; Z<=28; Z++) {
+    for (int N=28; N<=28; N++) {
+      nuclei.push_back(LDNucleus(Z, Z+N, eosInside).MakeUniquePtr());
+      nucleiStatic.push_back(
+          LDNucleus(Z, Z+N, eosInside).GetStaticNucleus().MakeUniquePtr());
+    }
   }
   EOSNSE nseEos(nuclei, eos);
+  EOSNSE nseEosStatic(nucleiStatic, eos);
   
   std::cout << "[1] n_{n,t} " << std::endl; 
   std::cout << "[2] n_{p,t} " << std::endl; 
   std::cout << "[3] n_{n,o} " << std::endl; 
   std::cout << "[4] n_{p,o} " << std::endl; 
   std::cout << "[5] sum v_i n_i " << std::endl; 
-  std::cout << "[6] E_c (first nucleus) " << std::endl; 
-  std::cout << "[7] mu_{n,o} " << std::endl; 
-  std::cout << "[8] mu_{p,o} " << std::endl; 
-  std::cout << "[9] P_o " << std::endl; 
-  double T = 0.2/HBC;
-  for (double np0 = 1.e-2; np0<1.e-1; np0 *= 1.2) {
+  std::cout << "[6] n_{n,t} (static) " << std::endl; 
+  std::cout << "[7] n_{p,t} (static) " << std::endl; 
+  std::cout << "[8] n_{n,o} (static) " << std::endl; 
+  std::cout << "[9] n_{p,o} (static) " << std::endl; 
+  std::cout << "[10] sum v_i n_i (static) " << std::endl; 
+  std::cout << "[11] E_c (first nucleus) " << std::endl; 
+  std::cout << "[12] mu_{n,o} " << std::endl; 
+  std::cout << "[13] mu_{p,o} " << std::endl; 
+  std::cout << "[14] P_o " << std::endl; 
+  std::cout << "[15] BE (first nucleus) " << std::endl; 
+  std::cout << "[16] BE (first nucleus, static) " << std::endl; 
+  std::cout << "[17] rho (first nucleus) " << std::endl; 
+  
+  double T = 1.5/HBC;
+  
+  for (double np0 = 1.e-2; np0<1.1e-2; np0 *= 1.2) {
     double nn0 = 1.e-20;
     double delta = 0.1;
     double npo = np0;
@@ -51,6 +61,8 @@ int main() {
         //    nseEos.GetTotalDensities(EOSData::InputFromTNnNp(T, nn0, np0)); 
         NSEProperties nse = 
             nseEos.GetExteriorProtonDensity(np0, nn0, T);
+        NSEProperties nseStat = 
+            nseEosStatic.GetExteriorProtonDensity(np0, nn0, T);
         double npt = nse.npTot;
         double nnt = nse.nnTot;
         double npe = nse.eosExterior.Np();
@@ -58,11 +70,20 @@ int main() {
          
         std::cout << nnt << " " << npt << " " 
           << nne << " " << npe << " " 
-          << nse.unuc << " " << 
+          << nse.unuc << " " 
+          << nseStat.nnTot << " " << nseStat.npTot << " " 
+          << nseStat.eosExterior.Nn() << " " << nseStat.eosExterior.Np() << " " 
+          << nseStat.unuc << " " << 
           nuclei[0]->CoulombEnergy(
           nuclei[0]->GetVolume(nse.eosExterior, npt), npe, npt)
           << " " << nse.eosExterior.Mun() << " " << nse.eosExterior.Mup() 
-          << " " << nse.eosExterior.P() << std::endl;
+          << " " << nse.eosExterior.P()  
+          << " " << nuclei[0]->GetBindingEnergy(nse.eosExterior, npt)
+          * 197.3 / (double) nuclei[0]->GetA()
+          << " " << nucleiStatic[0]->GetBindingEnergy(nse.eosExterior, npt)
+          * 197.3 / (double) nucleiStatic[0]->GetA()
+          << " " << nuclei[0]->GetA()/nuclei[0]->GetVolume(nse.eosExterior, npt)
+          << std::endl;
         if (fabs(nnt/nno - 1.0) > 0.02 || fabs(npt/npo - 1.0)>0.02) 
             delta *= 0.8;
         if (fabs((nnt+npt)/(npo + nno)-1.0) < 1.e-3) delta /= 0.8;
