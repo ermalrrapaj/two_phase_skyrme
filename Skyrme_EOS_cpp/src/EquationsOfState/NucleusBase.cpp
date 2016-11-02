97465f191cebeb20f1461ebc383406fdc699db37
ermal <ermalrrapaj@gmail.com>
2016-11-01 18:35:47 -0700

    nucleus base and surface term

--- Skyrme_EOS_cpp/src/EquationsOfState/NucleusBase.cpp
+++ Skyrme_EOS_cpp/src/EquationsOfState/NucleusBase.cpp
@@ -19,22 +19,28 @@ static double MNUC = 938.918/HBC;
 std::vector<double> StaticNucleus::CoulombEnergy(double v, double npo, 
     double ne) const {
   double Z = (double) NucleusBase::mZ; 
-  double u = v*(ne - npo) / (Z - v*npo);
-  double D = 0.5*u - 1.5*pow(u,1.0/3.0);
-  double udDduoD = (0.5*u- 0.5*pow(u,1.0/3.0))/(D + 1.e-40);
-  double dludv = 1.0/ (v + 1.e-40) + npo / (Z - v*npo);
-  double dludne = v / (ne - npo + 1.e-40);
-  double dludnpo = (u - 1.0) / (ne - npo + 1.e-40);
+  double u = v*(ne - npo) / (Z - v*npo + 1.e-80);
+  double D = 0.5*u - 1.5*pow(u,1.0/3.0) + 1.0;
+  double DprimeD = 0.5*(1.0- pow(u,-2.0/3.0))/(D + 1.e-80);
+  double dudlogne = ne*v / (Z - v*npo + 1.e-80);
+  double dudlogv = u*Z/(Z-v*npo+ 1.e-80);
+  double dudlognpo = u*npo*(1.0/(npo - ne + 1.e-80) + v/(Z-v*npo+1.e-80));
   double vn1o3 = pow(v, -1.0/3.0);
   double fac = 3.0 * Constants::ElementaryChargeSquared 
       / (5.0 * pow(3.0 / (4.0 * Constants::Pi), 1.0/3.0));
 
   double Ec      = fac * vn1o3 * D * pow(Z - npo*v, 2);
-  double dEcdv   = Ec * (udDduoD * dludv - 1.0/(3.0 * v) - 2.0 * npo / (Z-npo*v));
-  double dEcdnpo = Ec * (udDduoD * dludnpo - 2.0 * v / (Z-npo*v)); 
-  double dEcdne  = Ec *  udDduoD * dludne; 
+  double dEcdv   = Ec/v * (DprimeD*dudlogv - 1.0/3.0 - 2.0*npo*v/(Z-v*npo+1.e-80));
+  double dEcdlognpo = Ec * (DprimeD * dudlognpo - 2.0 * v*npo/(Z-npo*v+1.e-80)); 
+  double dEcdlogne  = Ec *  DprimeD * dudlogne; 
   
-  return {Ec, dEcdv, dEcdnpo, dEcdne};
+  std::vector<double> result(4);
+  result[0] = Ec;
+  result[1] = dEcdv;
+  result[2] = dEcdlognpo;
+  result[3] = dEcdlogne;
+  
+  return result;
 }
 
 double StaticNucleus::FreeEnergy(const EOSData& eosIn, double ne, double ni) const {
@@ -49,9 +55,13 @@ double StaticNucleus::Entropy (const EOSData& eosIn, double ne, double ni) const
 	return 0.0;
 }
 
+///
+/// Calculate the pressure from a single nucleus,
+/// to get the total pressure contribution -> ni * NucleusPressure
+///
 double StaticNucleus::NucleusPressure (const EOSData& eosIn, double ne, double uo) const{
   auto Ec = CoulombEnergy(GetVolume(eosIn, ne), eosIn.Np(), ne);
-  return eosIn.T() + Ec[3] + eosIn.Np()/(uo+1.e-80)*Ec[2]; 
+  return eosIn.T() + Ec.at(3) + eosIn.Np()/(uo+1.e-80)*Ec[2]; 
 }
 
 double StaticNucleus::Nucleusmup (const EOSData& eosIn, double ne, double uo, double ni) const {
